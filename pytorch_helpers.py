e135d4027eb0d1be48b71eb35f412892cec0872b
Senh Mo Chuang <senhmo@gmail.com>
2020-05-07 20:22:25 -0500

    improved helper functionality

--- pytorch_helpers.py
+++ pytorch_helpers.py
@@ -1,5 +1,6 @@
 from torch.utils import data
 from pandas import pd
+import torch
 
 class FeaturesLabelSplitter(data.Dataset):
     'Characterizes a dataset for PyTorch'
@@ -21,50 +22,3 @@ class FeaturesLabelSplitter(data.Dataset):
         y = (torch.LongTensor(self.dataframe[self.labels].to_numpy())).squeeze(1)[index]
 
         return X, y
-
-def train_test_split(dataframe, shuffle=False, test_decimal=.2):
-    assert test_decimal < 1
-    assert test_decimal > 0
-    if shuffle:
-        train_df = dataframe.sample(frac=(1-test_decimal))
-    else:
-        tt_boarder = int(dataframe.shape[0]*(1-test_decimal))
-        train_df = dataframe[:tt_boarder]
-    test_df = dataframe.drop(train_df.index)
-    return train_df, test_df
-
-def train_test_split_even(dataframe, label_name, shuffle=True, test_decimal=0.2, even_num_labels=True):
-    assert test_decimal < 1
-    assert test_decimal > 0
-    tt_boarder = int(dataframe.shape[0]*(1-test_decimal))
-    if even_num_labels:
-        min_sample = min(y_train.value_counts())
-        
-        train_df = pd.DataFrame(columns=dataframe.columns)
-        if tt_boarder >= min_sample*len(type_count)*(1-test_decimal):
-            if shuffle:
-                for each_type in dataframe[label_name].unique():
-                    train_df = train_df.append(dataframe[each_type == dataframe[label_name]].sample(frac=(1-test_decimal)))     
-            else:
-                for each_type in dataframe[label_name].unique():
-                    train_df = train_df.append(dataframe[each_type == dataframe[label_name]][:tt_boarder//len(type_count)])
-        else:
-            if shuffle:
-                for each_type in dataframe[label_name].unique():
-                    train_df = train_df.append(dataframe[each_type == dataframe[label_name]].sample(frac=(1-test_decimal)))
-            else:
-                for each_type in dataframe[label_name].unique():
-                    train_df = train_df.append(dataframe[each_type == dataframe[label_name]][:tt_boarder//len(type_count)])
-        test_df = dataframe.drop(train_df.index)
-    return train_df, test_df
-
-def my_hot_encoding(dataframe, feature_list_to_encode):
-    encoding_dict = {}
-    for each_feature in feature_list_to_encode:
-        feature_dict = {}
-        for index_type, each_type in enumerate(dataframe[each_feature].unique()):
-            feature_dict[each_type] = index_type
-        encoding_dict[each_feature] = feature_dict
-    for each_feature in feature_list_to_encode:
-        dataframe[each_feature] = dataframe[each_feature].map(lambda x: encoding_dict[each_feature][x])
-    return encoding_dict
---
+++ pytorch_test_train_helpers.py
@@ -0,0 +1,67 @@
+import torch
+import pandas as pd
+import numpy as np
+
+
+def even_distribution(dataframe: pd, label, my_sample_number=5, shuffle=True):
+    value_count_output = dataframe[label].value_counts()
+    if my_sample_number <= value_count_output.min():
+        unique_output_dict = value_count_output.to_dict()
+        dataframe_train_list = [dataframe[dataframe[label] == each_outcome].sample(my_sample_number) for each_outcome in unique_output_dict]
+        if shuffle:
+            dataframe_train_df = pd.concat(dataframe_train_list).sample(frac=1).copy()
+        else:
+            dataframe_train_df = pd.concat(dataframe_train_list).copy()
+        dataframe_test_df = dataframe.drop(dataframe_train_df.index).copy()
+        return dataframe_train_df, dataframe_test_df
+    else:
+        print(value_count_output)
+        print(dataframe.shape)
+        print('Resorting to normal output')
+        assert my_sample_number > dataframe.shape[0]
+        if shuffle:
+            dataframe_train_df = dataframe.sample(my_sample_number)
+        else:
+            dataframe_train_df = dataframe.head(my_sample_number)
+        return dataframe_train_df, dataframe_train_df.drop(dataframe_train_df.index)
+
+def my_hot_encoding(dataframe, feature_list_to_encode):
+    encoding_dict = {}
+    for each_feature in feature_list_to_encode:
+        feature_dict = {}
+        for index_type, each_type in enumerate(dataframe[each_feature].unique()):
+            feature_dict[each_type] = index_type
+        encoding_dict[each_feature] = feature_dict
+    for each_feature in feature_list_to_encode:
+        dataframe[each_feature] = dataframe[each_feature].map(lambda x: encoding_dict[each_feature][x])
+    dataframe[each_feature].astype(np.int64)
+    return encoding_dict        
+
+def train_test_split(dataframe, shuffle=False, test_decimal=.2):
+    assert test_decimal < 1
+    assert test_decimal > 0
+    if shuffle:
+        train_df = dataframe.sample(frac=(1-test_decimal))
+    else:
+        tt_boarder = int(dataframe.shape[0]*(1-test_decimal))
+        train_df = dataframe[:tt_boarder]
+    test_df = dataframe.drop(train_df.index)
+    return train_df, test_df
+
+if __name__=="__main__":
+    iris_main = pd.read_csv('https://raw.githubusercontent.com/pandas-dev/pandas/master/pandas/tests/data/iris.csv')
+    iris = iris_main.copy()
+    x, y = even_distribution(iris, 'Name', 5, False)
+    print(x, len(x))
+    print(y, len(y))
+    
+
+    unique_dict = my_hot_encoding(iris, ['Name'])
+    print(iris.dtypes)
+    print(unique_dict)
+    print(iris)
+    print(iris.dtypes)
+
+    a, b = train_test_split(iris)
+    print(a)
+    print(b)
